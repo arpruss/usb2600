be4ae4f0a33ee11117864aabb89932bd4b16a0fb
arpruss <arpruss@gmail.com>
2020-05-23 23:13:01 -0500

    support dual joystick

--- usb2600.ino
+++ usb2600.ino
@@ -7,47 +7,56 @@
 #include <USBComposite.h>
 #include "debounce.h"
 
+#define NUM_PROFILES 2 // support two joysticks or four paddles
+
 const uint32 paddleMaxResistance = 1000000;
 const uint32 paddleGroundResistor = 100000; // paddleGroundResistor should evenly divide paddleMaxResistance
 
 // designed for an stm32f103c8t6 blue pill
 
 // Atari pin 7 -- VCC
+// Controller #1
 // Atari pin 3 -- PA2 // left paddle button, joystick left
 // Atari pin 4 -- PA4 // right paddle button, joystick right
 // Atari pin 9 -- PA1 // pot 1
 // Atari pin 5 -- PA3 // pot 2
 // Atari pin 8 -- GND
-// Atari pin 6 -- PA5 // fire
-// Atari pin 1 -- PA6 // joystick up
-// Atari pin 2 -- PA7 // joystick down
+// Atari pin 6 -- PB5 // fire
+// Atari pin 1 -- PB3 // joystick up
+// Atari pin 2 -- PB4 // joystick down
+// 100K resistors between Atari pin 9 and GND and between Atari pin 5 and GND
+
+// Controller #2
+// Atari pin 3 -- PA6 // left paddle button, joystick left
+// Atari pin 4 -- PA8 // right paddle button, joystick right
+// Atari pin 9 -- PA5 // pot 1
+// Atari pin 5 -- PA7 // pot 2
+// Atari pin 8 -- GND
+// Atari pin 6 -- PB8 // fire
+// Atari pin 1 -- PB6 // joystick up
+// Atari pin 2 -- PB7 // joystick down
 // 100K resistors between Atari pin 9 and GND and between Atari pin 5 and GND
 
 #define PRODUCT_ID 0x4BA2
 #define LED PC13
-#define ANALOG1 PA1
-#define ANALOG2 PA3
-#define JOY_LEFT  PA2 // also left paddle button
-#define JOY_RIGHT PA4 // also right paddle paddle
-#define JOY_UP    PA6 
-#define JOY_DOWN  PA7
-#define JOY_FIRE  PA5
+// controller 1
+#define JOY1_POT1 PA1
+#define JOY1_POT2 PA3
+#define JOY1_LEFT  PA2 // also left paddle button
+#define JOY1_RIGHT PA4 // also right paddle paddle
+#define JOY1_UP    PB3 
+#define JOY1_DOWN  PB4
+#define JOY1_FIRE  PB5
 
-#undef SUPPORT_X360 // not fully supported
-#undef SERIAL_DEBUG
+// controller 2
+#define JOY2_POT1 PA5
+#define JOY2_POT2 PA7
+#define JOY2_LEFT  PA6 // also left paddle button
+#define JOY2_RIGHT PA8 // also right paddle paddle
+#define JOY2_UP    PB6 
+#define JOY2_DOWN  PB7
+#define JOY2_FIRE  PB8
 
-// at READ_ITERATIONS=1, 12 ms
-// 20: 12 ms
-// 40: 12 ms
-// 80: 12 ms
-// 100: 16 ms
-// 800: 36 ms
-
-#define NUM_EXTRA 0  // number of extra keys that emulate keyboard presses
-#if NUM_EXTRA
-const uint16 extraKeys[NUM_EXTRA] = { KEY_F1, KEY_F2, '[', ']' };
-#endif
-#define NUM_PADDLES 2
 #define HYSTERESIS 20 // shifts smaller than this are rejected
 #define MAX_HYSTERESIS_REJECTIONS 8 // unless we've reached this many of them, and then we use an average
 #define READ_ITERATIONS 80
@@ -115,8 +124,8 @@ class HIDSimpleJoystick : public HIDReporter {
     }
 };
 
-uint16 analogRead2(uint8 pin) {
-  return adc_read(pin == ANALOG1 ? &adc1 : &adc2, PIN_MAP[pin].adc_channel);
+uint16 analogRead2(uint8 pin, bool firstADC) {
+  return adc_read(firstADC ? &adc1 : &adc2, PIN_MAP[pin].adc_channel);
 }
 
 #ifdef SERIAL_DEBUG
@@ -132,12 +141,13 @@ class AnalogPort {
     uint32 oldValue = NO_VALUE;
     uint32 rejectedCount = 0;
     uint32 rejectedSum = 0;
+    bool firstADC = true;
     uint32 getValue() {
       uint32 v = 0;
 
 //      nvic_globalirq_disable();
       for (uint32 i = 0 ; i < READ_ITERATIONS ; i++)
-        v += analogRead2(port);
+        v += analogRead2(port, firstADC);
 //      nvic_globalirq_enable();
       v = (v + READ_ITERATIONS / 2) / READ_ITERATIONS;
 
@@ -167,43 +177,127 @@ class AnalogPort {
       return 4095 - v;
     };
 
-    AnalogPort(uint32 _port) {
+    AnalogPort(uint32 _port, bool _firstADC) {
       port = _port;
+      firstADC = _firstADC;
     };
 };
 
-AnalogPort analog1(ANALOG1);
-AnalogPort analog2(ANALOG2);
-AnalogPort* analog[2] = { &analog1, &analog2 };
-Debounce digital1(PA2);
-Debounce digital2(PA4);
-#define NUM_DIGITAL 5
-Debounce joyLeft(JOY_LEFT);
-Debounce joyRight(JOY_RIGHT);
-Debounce joyUp(JOY_UP);
-Debounce joyDown(JOY_DOWN);
-Debounce joyFire(JOY_FIRE); 
-Debounce* digital[NUM_DIGITAL] = { &joyLeft, &joyRight, &joyUp, &joyDown, &joyFire };
-bool havePaddle = false;
-
 USBHID HID;
-HIDSimpleJoystick joy(HID);
+
+class JoystickProfile {
+public:
+    HIDSimpleJoystick joy;
+    AnalogPort analog1;
+    AnalogPort analog2;
+    AnalogPort* analog[2] = {&analog1,&analog2};
+    Debounce joyLeft;
+    Debounce joyRight;
+    Debounce joyUp;
+    Debounce joyDown;
+    Debounce joyFire;
+    Debounce* digital[5] = { &joyLeft, &joyRight, &joyUp, &joyDown, &joyFire };
+    bool havePaddle = false;
+
+    void setup() {
+      for (uint32 i = 0 ; i < 2; i++) 
+        pinMode(analog[i]->port, INPUT_ANALOG);
+    
+      for (uint32 i = 0 ; i < sizeof(digital)/sizeof(*digital) ; i++) 
+        pinMode(digital[i]->pin, INPUT_PULLDOWN);    
+    }
+
+    uint32 valueToPot(uint32 value) {
+      // value = (4095 * potResistance + 0 * paddleGroundResistor) / (potResistance + paddleGroundResistor)
+      // value * (potResistance + paddleGroundResistor) = 4095 * potResistance
+      // (4095 - value) * potResistance = value * paddleGroundResistor
+      // potResistance = value * paddleGroundResistor / (4095 - value)
+      // axis = potResistance / paddleMaxResistance * 1023
+      // axis = 1023 * value * paddleGroundResistor / (4095 - value) / paddleMaxResistance
+    
+      if (value == 4095)
+        return 1023;
+      return 1023 * value / ((4095 - value) * paddleMaxResistance / paddleGroundResistor);
+    }
+    
+    void loop() {
+      uint32 pots[2];
+    
+      havePaddle = false;
+      for (uint32 i = 0 ; i < 2; i++ ) {
+        uint32 value = analog[i]->getValue();
+        if (value > 4095/(paddleMaxResistance/paddleGroundResistor)/2) {
+          havePaddle = true;
+          digitalWrite(LED, 0);
+        }
+        pots[i] = valueToPot(value);
+      }
+    
+      if (havePaddle) {
+        if (joyLeft.getState())
+          joy.joyReport.button1 = 1;
+        else
+          joy.joyReport.button1 = 0;
+        if (joyRight.getState())
+          joy.joyReport.button2 = 1;
+        else
+          joy.joyReport.button2 = 0;
+        joy.joyReport.x = pots[0];
+        joy.joyReport.y = pots[1];
+      }
+      else {
+        bool left = joyLeft.getState();
+        bool right = joyRight.getState();
+        bool up = joyUp.getState();
+        bool down = joyDown.getState();
+        bool fire = joyFire.getState();
+    
+        if (left) 
+          joy.joyReport.x = 0;
+        else if (right)
+          joy.joyReport.x = 1023;
+        else
+          joy.joyReport.x = 512;
+    
+        if (up)
+          joy.joyReport.y = 0;
+        else if (down)
+          joy.joyReport.y = 1023;
+        else
+          joy.joyReport.y = 512;
+    
+        if (fire) 
+          joy.joyReport.button1 = 1;
+        else
+          joy.joyReport.button1 = 0;
+      }
+      
+      joy.sendReport();
+    }
+    
+    JoystickProfile(uint32 left, uint32 right, uint32 up, uint32 down, uint32 fire, uint32 pot1, uint32 pot2) :
+        joy(HID), analog1(pot1, true), analog2(pot2, false), joyLeft(left), joyRight(right), joyUp(up), joyDown(down), joyFire(fire) {}
+};
+
+JoystickProfile joy1(JOY1_LEFT,JOY1_RIGHT,JOY1_UP,JOY1_DOWN,JOY1_FIRE,JOY1_POT1,JOY1_POT2);
+#if NUM_PROFILES > 1
+JoystickProfile joy2(JOY2_LEFT,JOY2_RIGHT,JOY2_UP,JOY2_DOWN,JOY2_FIRE,JOY2_POT1,JOY2_POT2);
+JoystickProfile* profiles[] = { &joy1, &joy2 };
+#else
+JoystickProfile* profiles[] = { &joy1 };
+#endif
 
 void setup() {
   // default is 55_5
   adc_set_sample_rate(ADC1, ADC_SMPR_239_5);
   adc_set_sample_rate(ADC2, ADC_SMPR_239_5);
 
-  for (uint32 i = 0 ; i < 2; i++) 
-    pinMode(analog[i]->port, INPUT_ANALOG);
-
-  for (uint32 i = 0 ; i < NUM_DIGITAL ; i++) 
-    pinMode(digital[i]->pin, INPUT_PULLDOWN);
+  for (uint32 i=0;i<NUM_PROFILES;i++)
+    profiles[i]->setup();
 
   pinMode(LED, OUTPUT);
   digitalWrite(LED, 1);
 
-  HID.clear();
   USBComposite.setVendorId(0x04d8);
   USBComposite.setProductId(0xbeef);
   USBComposite.setManufacturerString("Grand Idea Studio");
@@ -213,71 +307,8 @@ void setup() {
   while (!USBComposite);
 }
 
-uint32 valueToPot(uint32 value) {
-  // value = (4095 * potResistance + 0 * paddleGroundResistor) / (potResistance + paddleGroundResistor)
-  // value * (potResistance + paddleGroundResistor) = 4095 * potResistance
-  // (4095 - value) * potResistance = value * paddleGroundResistor
-  // potResistance = value * paddleGroundResistor / (4095 - value)
-  // axis = potResistance / paddleMaxResistance * 1023
-  // axis = 1023 * value * paddleGroundResistor / (4095 - value) / paddleMaxResistance
-
-  if (value == 4095)
-    return 1023;
-  return 1023 * value / ((4095 - value) * paddleMaxResistance / paddleGroundResistor);
-}
-
 void loop() {
-  uint32 pots[2];
-
-  havePaddle = false;
-  for (uint32 i = 0 ; i < 2; i++ ) {
-    uint32 value = analog[i]->getValue();
-    if (value > 4095/(paddleMaxResistance/paddleGroundResistance)/2) {
-      havePaddle = true;
-      digitalWrite(LED, 0);
-    }
-    pots[i] = valueToPot(value);
-  }
-
-  if (havePaddle) {
-    if (joyLeft.getState())
-      joy.joyReport.button1 = 1;
-    else
-      joy.joyReport.button1 = 0;
-    if (joyRight.getState())
-      joy.joyReport.button2 = 1;
-    else
-      joy.joyReport.button2 = 0;
-    joy.joyReport.x = pots[0];
-    joy.joyReport.y = pots[1];
-  }
-  else {
-    bool left = joyLeft.getState();
-    bool right = joyRight.getState();
-    bool up = joyUp.getState();
-    bool down = joyDown.getState();
-    bool fire = joyFire.getState();
-
-    if (left) 
-      joy.joyReport.x = 0;
-    else if (right)
-      joy.joyReport.x = 1023;
-    else
-      joy.joyReport.x = 512;
-
-    if (up)
-      joy.joyReport.y = 0;
-    else if (down)
-      joy.joyReport.y = 1023;
-    else
-      joy.joyReport.y = 512;
-
-    if (fire) 
-      joy.joyReport.button1 = 1;
-    else
-      joy.joyReport.button1 = 0;
-  }
-  
-  joy.sendReport();
+  for (uint32 i=0;i<NUM_PROFILES;i++)
+    profiles[i]->loop();
 }
 
